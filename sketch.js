3ec68c812d556e8aa50337e697afad0174dc4b91
Betül <69481739+FatmaBetulUyar@users.noreply.github.com>
2023-08-09 13:29:30 +0300

    added rainbow,gray and eraser buttons.

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function createButtonWithClass(className, content = "") {
 
 // 1 header
 const headerDiv = createDivWithClass("header");
-headerDiv.textContent="Etch a Scretch";
+headerDiv.textContent = "Etch a Scretch";
 containerDiv.appendChild(headerDiv);
 
 // 1 big content
@@ -27,9 +27,8 @@ containerDiv.appendChild(bigContentDiv);
 const menuDiv = createDivWithClass("menu");
 bigContentDiv.appendChild(menuDiv);
 
-
 const colorHeaderDiv = createDivWithClass("colorHeader");
-colorHeaderDiv.textContent="Choose Your Color";
+colorHeaderDiv.textContent = "Choose Your Color";
 menuDiv.appendChild(colorHeaderDiv);
 
 // 3 color button
@@ -39,7 +38,7 @@ colorInput.classList.add("form-control", "form-control-color", "color");
 colorInput.id = "exampleColorInput";
 colorInput.value = "black"; // Set a default color value
 colorInput.title = "Choose your color";
-colorInput.textContent="chose"
+colorInput.textContent = "chose";
 menuDiv.appendChild(colorInput);
 
 // 3 rainbow button
@@ -49,8 +48,13 @@ menuDiv.appendChild(rainbowButton);
 
 // 3 gray button
 const grayButton = createButtonWithClass("gray");
-grayButton.textContent = "clear";
+grayButton.textContent = "gray";
 menuDiv.appendChild(grayButton);
+
+// 3 clear button
+const clearButton = createButtonWithClass("clear");
+clearButton.textContent = "clear";
+menuDiv.appendChild(clearButton);
 
 // 3 eraser button
 const eraserButton = createButtonWithClass("eraser");
@@ -80,12 +84,20 @@ rangeInput.min = 1;
 rangeInput.max = 7;
 rangeInput.step = 1;
 rangeInput.style.accentColor = "#ACFADF";
+rangeInput.defaultValue = 7;
 rangeInput.style.border = "none";
+rangeInput.value = 7;
 scrollDiv.appendChild(rangeInput);
+let selectedValue = 7;
+let eraserClicked = false;
+let clearClicked = false;
+let rainbowClicked = false;
+let grayClicked = false;
+updateSketchDiv();
 
-rangeInput.addEventListener("input", () => {
+function updateSketchDiv(params) {
   sketchDiv.innerHTML = "";
-  let selectedValue = rangeInput.value;
+
   let pixelNumber = getPixelNumber(selectedValue);
   for (let i = 0; i < pixelNumber; i++) {
     for (let j = 0; j < pixelNumber; j++) {
@@ -95,12 +107,42 @@ rangeInput.addEventListener("input", () => {
       div.addEventListener("mouseenter", () => {
         //div.classList.add("hover");
         div.style.backgroundColor = colorInput.value;
+
+        if (eraserClicked) {
+          div.style.backgroundColor = "white";
+        }
+        if (rainbowClicked) {
+          div.style.backgroundColor = getRandomColor();
+        }
+        if (grayClicked) {
+          div.style.backgroundColor = getRandomGrayColors();
+        }
       });
       sketchDiv.appendChild(div);
 
       console.log(selectedValue);
     }
   }
+}
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function getRandomGrayColors() {
+  const randomShade = Math.floor(Math.random() * 256); // 0 ile 255 arasında rastgele bir değer
+  const grayColor = `rgb(${randomShade}, ${randomShade}, ${randomShade})`;
+  return grayColor;
+}
+
+rangeInput.addEventListener("input", () => {
+  selectedValue = rangeInput.value;
+  eraserClicked = false;
+  updateSketchDiv();
 });
 
 function getPixelNumber(selectedValue) {
@@ -131,8 +173,33 @@ function getPixelNumber(selectedValue) {
   return pixelNumber;
 }
 
-eraserButton.addEventListener("mouseenter", () => {});
+clearButton.addEventListener("click", () => {
+  clearClicked = true;
+  if (clearClicked) {
+    sketchDiv.innerHTML = "";
+  }
+  updateSketchDiv();
+});
+eraserButton.addEventListener("click", () => {
+  eraserClicked = true;
+  rainbowClicked = false;
+  clearClicked = false;
+  grayClicked = false;
+});
 
-// to do
-//add rainbow button colorful hover
-//add eraser button event listener
+rainbowButton.addEventListener("click", () => {
+  rainbowClicked = true;
+  clearClicked = false;
+  grayClicked = false;
+});
+
+grayButton.addEventListener("click", () => {
+  grayClicked = true;
+});
+
+colorInput.addEventListener("click", () => {
+  eraserClicked = false;
+  rainbowClicked = false;
+  clearClicked = false;
+  grayClicked = false;
+});
